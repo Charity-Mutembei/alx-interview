bb3722ac580059565fe8f506c579f7d5928a388c
Charity-Mutembei <charity.softwaredev@gmail.com>
2024-07-04 23:21:20 +0300

    task 0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,44 +1,38 @@
 #!/usr/bin/python3
 """
-Task 0
+Function returning winner in a prime game
 """
 
 
-def sieve(n):
-    """Return a list of primes up to n using the Sieve of Eratosthenes"""
-    is_prime = [True] * (n + 1)
-    p = 2
-    while p * p <= n:
-        if is_prime[p]:
-            for i in range(p * p, n + 1, p):
-                is_prime[i] = False
-        p += 1
-    primes = [p for p in range(2, n + 1) if is_prime[p]]
-    return primes
+def set_primes(k):
+    """Function returning a list of primes up to k"""
+    prime = []
+    sieve = [True] * (k + 1)
+    for p in range(2, k + 1):
+        if sieve[p]:
+            prime.append(p)
+            for i in range(p, k + 1, p):
+                sieve[i] = False
+    return prime
 
 
 def isWinner(x, nums):
-    max_n = max(nums)
-    primes = sieve(max_n)
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        prime_count = 0
-        for p in primes:
-            if p > n:
-                break
-            prime_count += 1
-
-        if prime_count % 2 == 0:
-            ben_wins += 1
-        else:
-            maria_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    """Function returning winner in a prime game"""
+    if x is None or x == 0 or nums is None or nums == []:
         return None
+    Maria = 0
+    Ben = 0
+
+    for i in range(x):
+        prime_number = set_primes(nums[i])
+
+        if len(prime_number) % 2 == 0:
+            Ben += 1
+        else:
+            Maria += 1
+
+    if Ben > Maria:
+        return "Ben"
+    elif Maria > Ben:
+        return "Maria"
+    return None
