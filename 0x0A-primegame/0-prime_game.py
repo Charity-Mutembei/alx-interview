0c431644999296625c073f5c2ea4ff645822796c
Charity-Mutembei <charity.softwaredev@gmail.com>
2024-07-04 22:40:20 +0300

    task 0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,45 +6,46 @@ trial question
 """
 
 
+def sieve_of_eratosthenes(limit):
+    sieve = [True] * (limit + 1)
+    sieve[0] = sieve[1] = False
+    p = 2
+    while (p * p <= limit):
+        if sieve[p]:
+            for i in range(p * p, limit + 1, p):
+                sieve[i] = False
+        p += 1
+    return [p for p in range(limit + 1) if sieve[p]]
+
+
+def winner_for_round(n, primes):
+    if n == 1:
+        return 'Ben'
+
+    count = 0
+    for prime in primes:
+        if prime > n:
+            break
+        count += 1
+
+    if count % 2 == 1:
+        return 'Maria'
+    else:
+        return 'Ben'
+
+
 def isWinner(x, nums):
     if not nums or x < 1:
         return None
 
     max_n = max(nums)
 
-    def sieve_of_eratosthenes(limit):
-        sieve = [True] * (limit + 1)
-        sieve[0] = sieve[1] = False
-        p = 2
-        while (p * p <= limit):
-            if sieve[p]:
-                for i in range(p * p, limit + 1, p):
-                    sieve[i] = False
-            p += 1
-        return [p for p in range(limit + 1) if sieve[p]]
-
     primes = sieve_of_eratosthenes(max_n)
-
-    def winner_for_round(n):
-        if n == 1:
-            return 'Ben'
-
-        count = 0
-        for prime in primes:
-            if prime > n:
-                break
-            count += 1
-
-        if count % 2 == 1:
-            return 'Maria'
-        else:
-            return 'Ben'
-
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        winner = winner_for_round(n)
+        winner = winner_for_round(n, primes)
         if winner == 'Maria':
             maria_wins += 1
         elif winner == 'Ben':
